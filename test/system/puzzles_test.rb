b0d519be59be06f087e41412a1ad1eb4f36aa2ff
Alison Furlong <furlong.24@osu.edu>
2024-11-25 18:52:04 +0000

    forgot the system tests

--- test/system/puzzles_test.rb
+++ test/system/puzzles_test.rb
@@ -10,34 +10,4 @@ class PuzzlesTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Puzzles"
   end
 
-  test "should create puzzle" do
-    visit puzzles_url
-    click_on "New puzzle"
-
-    fill_in "Author", with: @puzzle.author
-    fill_in "Publication date", with: @puzzle.publication_date
-    click_on "Create Puzzle"
-
-    assert_text "Puzzle was successfully created"
-    click_on "Back"
-  end
-
-  test "should update Puzzle" do
-    visit puzzle_url(@puzzle)
-    click_on "Edit this puzzle", match: :first
-
-    fill_in "Author", with: @puzzle.author
-    fill_in "Publication date", with: @puzzle.publication_date
-    click_on "Update Puzzle"
-
-    assert_text "Puzzle was successfully updated"
-    click_on "Back"
-  end
-
-  test "should destroy Puzzle" do
-    visit puzzle_url(@puzzle)
-    click_on "Destroy this puzzle", match: :first
-
-    assert_text "Puzzle was successfully destroyed"
-  end
 end
--- test/system/words_test.rb
+++ test/system/words_test.rb
@@ -10,34 +10,4 @@ class WordsTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Words"
   end
 
-  test "should create word" do
-    visit words_url
-    click_on "New word"
-
-    fill_in "Name", with: @word.name
-    fill_in "Order", with: @word.order
-    click_on "Create Word"
-
-    assert_text "Word was successfully created"
-    click_on "Back"
-  end
-
-  test "should update Word" do
-    visit word_url(@word)
-    click_on "Edit this word", match: :first
-
-    fill_in "Name", with: @word.name
-    fill_in "Order", with: @word.order
-    click_on "Update Word"
-
-    assert_text "Word was successfully updated"
-    click_on "Back"
-  end
-
-  test "should destroy Word" do
-    visit word_url(@word)
-    click_on "Destroy this word", match: :first
-
-    assert_text "Word was successfully destroyed"
-  end
 end
